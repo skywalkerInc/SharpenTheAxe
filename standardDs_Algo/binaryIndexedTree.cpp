0c71fd8f73c64beccaf20b221b29b5d060196ed1
kawaljeet <kawal4tech@gmail.com>
2021-06-29 16:03:11 +0530

    binaryIndexedTree clean

--- standardDs_Algo/binaryIndexedTree.cpp
+++ standardDs_Algo/binaryIndexedTree.cpp
@@ -4,27 +4,23 @@ using namespace std;
 class BIT {
 public:
     vector<int> biTree;
-    BIT(vector<int>&arr): biTree{vector<int>(arr.size()+1, 0)} {
-        for(int i=0; i<arr.size(); i++) {
+    BIT(vector<int>&arr): biTree{vector<int>(arr.size() +1, 0)} {
+        for (int i=0;i<arr.size(); i++) {
             update(i, arr[i]);
         }
     }
 
     int getSum(int qi) {
-        if (qi < 0 || qi>=biTree.size()) return INT_MIN;
-
+        qi += 1;
         int sum = 0;
-        while (qi >0 ) {
+        while (qi > 0) {
             sum += biTree[qi];
             qi -= qi & (-qi);
         }
         return sum;
     }
     bool update(int qi, int diff) {
-        if (qi<0 || qi>=biTree.size()) return false;
-
         qi += 1;
-
         while (qi < biTree.size()) {
             biTree[qi] += diff;
             qi += qi & (-qi);
@@ -46,9 +42,9 @@ void testBIT() {
     vector<int> arr = {0,1,2,3,4,5,6};
     BIT biTree = BIT(arr);
     biTree.print();
-    cout << "[0,4): " << biTree.getSum(4) << endl;
+    cout << "[0,4]: " << biTree.getSum(4) << endl;
     biTree.update(2, 3);
-    cout << "[0,4): " << biTree.getSum(4) << endl;
+    cout << "[0,4]: " << biTree.getSum(4) << endl;
 }
 
 int main() {
