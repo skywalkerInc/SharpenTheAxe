4e93fffad3e8c5a474b0b125c7dd807492c445fc
kawaljeet <kawal4tech@gmail.com>
2021-06-29 15:54:53 +0530

    added binary Indexed Tree

---
+++ standardDs_Algo/binaryIndexedTree.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BIT {
+public:
+    vector<int> biTree;
+    BIT(vector<int>&arr): biTree{vector<int>(arr.size()+1, 0)} {
+        for(int i=0; i<arr.size(); i++) {
+            update(i, arr[i]);
+        }
+    }
+
+    int getSum(int qi) {
+        if (qi < 0 || qi>=biTree.size()) return INT_MIN;
+
+        int sum = 0;
+        while (qi >0 ) {
+            sum += biTree[qi];
+            qi -= qi & (-qi);
+        }
+        return sum;
+    }
+    bool update(int qi, int diff) {
+        if (qi<0 || qi>=biTree.size()) return false;
+
+        qi += 1;
+
+        while (qi < biTree.size()) {
+            biTree[qi] += diff;
+            qi += qi & (-qi);
+        }
+        return true;
+    }
+    
+    void print() {
+        for (auto &ele: biTree) {
+            cout << ele << ", ";
+        }
+        cout << endl;
+    }
+
+};
+
+
+void testBIT() {
+    vector<int> arr = {0,1,2,3,4,5,6};
+    BIT biTree = BIT(arr);
+    biTree.print();
+    cout << "[0,4): " << biTree.getSum(4) << endl;
+    biTree.update(2, 3);
+    cout << "[0,4): " << biTree.getSum(4) << endl;
+}
+
+int main() {
+    testBIT();
+}
--- standardDs_Algo/segmentTree.cpp
+++ standardDs_Algo/segmentTree.cpp
@@ -9,66 +9,54 @@ class SegmentTree {
 public:
     vector<int> segTree;
     int arraySize;
+
     SegmentTree(vector<int>& arr) {
         arraySize = arr.size();
-        int segTreeHeight = (int)(ceil(log2(arraySize)));
-        int segTreeLen = 2*(int)pow(2, segTreeHeight) - 1;
-    
-        segTree = vector<int>(segTreeLen,0);
+        int stHeight = (int)(ceil(log2(arraySize)));
+        int stSize = 2*(int)pow(2, stHeight) - 1;
+        segTree = vector<int>(stSize);
         segTreeUtil(arr, 0, arraySize-1, 0);
     }
     int segTreeUtil(vector<int>& arr, int ss, int se, int si) {
-        
         if (ss == se) {
             segTree[si] = arr[ss];
             return segTree[si];
         }
 
         int mid = ss + (se-ss)/2;
-        segTree[si] = segTreeUtil(arr, ss, mid, 2*si+1) +
-                            segTreeUtil(arr, mid+1, se, 2*si+2);
-
+        segTree[si] = segTreeUtil(arr, ss, mid, 2*si+1) + segTreeUtil(arr, mid+1, se, 2*si+2);
         return segTree[si];
     }
 
     int getSumUtil(int qs, int qe, int ss, int se, int si) {
-        // st lies in qrange
-        // st is completely outside qrange
-        // st is partially inside qrange
-
-        if (se < qs || ss > qe) return 0;
-        
+        if (ss > qe || se < qs) return 0;
         if (ss >= qs && se <= qe) return segTree[si];
 
         int mid = ss + (se-ss)/2;
-        return getSumUtil(qs, qe, ss, mid, 2*si +1) + 
-                getSumUtil(qs, qe, mid+1, se, 2*si+2);
+        return getSumUtil(qs, qe, ss, mid, 2*si+1) + getSumUtil(qs, qe, mid+1, se, 2*si+2);
     }
 
-    void updateUtil(int ss, int se, int currIndex, int qi, int diff) {
-        // qi lies inside ss, se
-        // qi lies outside ss, se
-
-        if (qi < ss || qi > se) return;
-
-        segTree[currIndex] += diff;
+    void updateUtil(int ss, int se, int si, int index, int diff) {
+        if (index<ss || index>se) return;
         
+        segTree[si] += diff;
+
         if (ss != se) {
             int mid = ss + (se-ss)/2;
-            updateUtil(ss, mid, 2*currIndex+1, qi, diff);
-            updateUtil(mid+1, se, 2*currIndex+2, qi, diff);
+            updateUtil(ss, mid, 2*si+1, index, diff);
+            updateUtil(mid+1, se, 2*si+2, index, diff);
         }
     }
 
     int getSum(int qs, int qe) {
-        
-        if (qs<0 || qe>= arraySize || qe < qs) return INT_MIN;
+        if (qs < 0 || qe >= arraySize || qe<qs) return INT_MIN;
 
         return getSumUtil(qs, qe, 0, arraySize-1, 0);
     } 
-    bool update(int index, int diff) {
-        if (index < 0 || index >= arraySize) return false;
-        updateUtil(0, arraySize -1, 0, index, diff);
+    bool update(vector<int>& arr, int index, int diff) {
+        if (index < 0 || index>=arraySize) return false;
+        arr[index] += diff;
+        updateUtil(0, arraySize-1, 0, index, diff);
         return true;
     }
 
@@ -80,12 +68,15 @@ public:
     }
 };
 
-
-int main() {
+void testSegTree() {
     vector<int> arr = {0,1,2,3,4,5,6};
     SegmentTree segTree = SegmentTree(arr);
     segTree.print();
     cout << "[2,5]: " << segTree.getSum(2,5) << endl;
-    segTree.update(2,3);
+    segTree.update(arr, 2,3);
     cout << "[2,5]: " << segTree.getSum(2,5) << endl;
+}
+
+int main() {
+    testSegTree()
 }
